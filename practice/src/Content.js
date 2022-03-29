3cd7679bac02b6ab1d82c106975ea88a8d5855a2
Drew-Trumbaturi <30042619@student.toiohomai.ac.nz>
2022-03-30 10:25:35 +1300

    first commit

---
+++ practice/src/Content.js
@@ -0,0 +1,26 @@
+import Monsters from './Data.json';
+
+const monsters = Monsters.map(
+    (Monster) => {
+        return (
+            <div key={Monsters.monster}> 
+                <h4>{Monster.monster}</h4>
+                <p>Risk: {Monster.risk}</p>
+                <p>Location: {Monster.location}</p>
+                <p><img src={Monster.image} /></p>
+                <hr />
+            </div>
+        );
+    }
+);
+
+function Content() {
+    return (
+        <div>
+            <h1>Content</h1>
+            {monsters}
+        </div>
+    );
+}
+
+export default Content;
